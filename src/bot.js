bc04261a089cf0d364ce849229b7639d70ed3039
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-09-03 22:14:12 +0200

    deleted express

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,5 @@
 require('dotenv').config();
-var express = require('express');
-var app = express();
-const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+
 
 const { Client, MessageAttachment } = require('discord.js');
 
